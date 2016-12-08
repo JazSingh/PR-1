bd7615ba05418845705fe78a1e22512f6f17bd25
Daniël Stekelenburg <d.stekelenbug@afas.nl>
2016-12-08 11:43:37 +0100

    Try to implement # of planes in a digit

--- functions.R
+++ functions.R
@@ -22,3 +22,92 @@ getDigits <- function(digitData) {
   as.factor(digitData$label)
 }
 
+getMinimum <- function(digitData) {
+  return(1)   
+}
+
+#function to determine neighbours which are inactive by looking in all 8 directions
+inactiveNeighbours <- function(x, y, digitData){
+  inactiveNeighbours = list()
+  w = 28
+  h = 28
+  
+  wmin = x - 1 
+  wmax = x + 1 
+  hmin = y - 1 
+  hmax = y + 1
+  
+return(inactiveNeighbours)
+}
+
+Queue <- setRefClass(Class = "Queue",
+   fields = list(
+     name = "character",
+     data = "list"
+   ),
+   methods = list(
+     size = function() {
+       'Returns the number of items in the queue.'
+       return(length(data))
+     },
+     #
+     push = function(item) {
+       'Inserts element at back of the queue.'
+       data[[size()+1]] <<- item
+     },
+     #
+     pop = function() {
+       'Removes and returns head of queue (or raises error if queue is empty).'
+       if (size() == 0) stop("queue is empty!")
+       value <- data[[1]]
+       data[[1]] <<- NULL
+       value
+     },
+     #
+     poll = function() {
+       'Removes and returns head of queue (or NULL if queue is empty).'
+       if (size() == 0) return(NULL)
+       else pop()
+     },
+     #
+     peek = function(pos = c(1)) {
+       'Returns (but does not remove) specified positions in queue (or NULL if any one of them is not available).'
+       if (size() < max(pos)) return(NULL)
+       #
+       if (length(pos) == 1) return(data[[pos]])
+       else return(data[pos])
+     },
+     initialize=function(...) {
+       callSuper(...)
+       #
+       # Initialise fields here (place holder)...
+       #
+       .self
+     }
+   )
+)
+
+getPlanes <- function(digitData) {
+  #number of planes of inactive pixels
+  visited = Counter() #counter to store already visited pixels
+  planes = 0
+  for( x in 1:28) {
+    for(y in 1:28) {
+    if (digitData[x+28*y] == 0 and visited[x+28*y] == 0){ #if a non-active has never been visited perform a bfs the get all adjacent inactive pixels
+    #print "=====New Plane=====", (x,y)
+    planes += 1
+    }
+  Queue = Queue()
+  Queue.push(x+28*y) #initialize bfs-visit
+  visited[x+28*y] = 1
+  while not Queue.isEmpty(): #BFS-visit to find all adjacent inactive pixels
+    current = Queue.pop()
+  successors = list()
+  for (z in successors) {
+    if visited[z] == 0:
+    Queue.push(z)
+  }
+  visited[z] = 1 
+    }
+  }
+}
